bb43e4550cafcbe976f396cf1c88996030690cf6
Paco <fjgramirez@abdprof.com>
2018-12-02 13:18:25 +0100

    jugando con la bbdd

--- server.js
+++ server.js
@@ -165,7 +165,7 @@ app.get('/dameUsuarios', function(req, res) {
         initDb(function(err) {});
     }
     if (db) {
-        db.collection('usuarios').findOne({}, function(err, res) {
+        db.collection('usuarios').findOne({ nombre: nombre }, function(err, res) {
             res.json('{ nombre: ' + res.nombre + '}');
         });
     } else {
